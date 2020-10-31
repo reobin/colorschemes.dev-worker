d483f85528c0891ec8a19f506c7aee5b2fd7f5ac
Robin Gagnon <me@reobin.dev>
2020-10-31 08:34:37 -0400

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name = "vimcolorschemes/worker",
-    version = "1.0.0",
-    description = "I fetch vim color scheme repositories and store them. That's about it.",
-    url = "https://github.com/vimcolorschemes/worker",
-    author = "Robin Gagnon",
-    author_email = "me@reobin.dev",
-    python_requires = ">=3.5",
-    # packages = find_packages(),
-    install_requires = [
-        "requests", # HTTP requests
-        "pymongo", # MongoDB client
-        "dnspython", # used by pymongo
-        "pytz", # timezones
+    name="vimcolorschemes/worker",
+    version="1.0.0",
+    description="I fetch vim color scheme repositories and store them. That's about it.",
+    url="https://github.com/vimcolorschemes/worker",
+    author="Robin Gagnon",
+    author_email="me@reobin.dev",
+    python_requires=">=3.5",
+    install_requires=[
+        "requests",  # HTTP requests
+        "pymongo",  # MongoDB client
+        "dnspython",  # used by pymongo
+        "pytz",  # timezones
     ],
-    extras_require = {
+    extras_require={
         "dev": [
             "pip-tools",
             "black",
         ],
     },
-    project_urls = {
+    project_urls={
         "Source": "https://github.com/vimcolorschemes/worker",
         "Bug Reports": "https://github.com/vimcolorschemes/worker/issues",
         "Website": "https://vimcolorschemes.com",
